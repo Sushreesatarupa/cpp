2b2d051ebb2531d0f7c3490cd236d4346951e589
Sushree Satarupa <64991656+Sushreesatarupa@users.noreply.github.com>
2021-08-19 11:51:47 +0530

    Create comments.cpp

---
+++ Fundamentals in C++/comments.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+ // This is a comment
+cout << "Hello World!";
+cout << "Hello World!"; // This is a comment
+/* The code below will print the words Hello World!
+to the screen, and it is amazing */
+cout << "Hello World!";
+//   cout << "Hello World!";
+//   cout << "Hello World!";
+//   cout << "Hello World!";
+//   cout << "Hello World!";
+//   cout << "Hello World!";
+  
+ return 0;
+}
